e1b4d7355365347c6c8bfe11dd8f38440f4f1491
Omar Yousry Alanssary <omaralanassry@gmail.com>
2025-11-29 10:29:48 +0200

    I have creare controller for the patient (get only)

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,42 @@
+﻿using clinical.APIs.Data;
+using clinical.APIs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace clinical.APIs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PatientController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPatient()
+        {
+            var patients = await _context.Patients.ToListAsync();
+            if (patients==null||patients.Count==0)
+            {
+                return NotFound();
+
+            }
+            return Ok(patients);
+        }
+
+        [HttpGet("{Patient_ID}")]
+        public async Task<IActionResult> GetPatientById(int Patient_ID)
+        {
+            var patient = await _context.Patients.FirstOrDefaultAsync(p => p.Patient_ID == Patient_ID);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
+        }
+    }
+}
